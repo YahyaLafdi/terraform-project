cc7cbbb4170931ec34954046f361ee427b1e9b25
Ubuntu <ubuntu@ip-172-31-39-165.eu-west-3.compute.internal>
2023-11-07 16:54:07 +0000

    creating an instance with existing sec group

--- main.tf
+++ main.tf
@@ -13,3 +13,24 @@ provider "aws" {
     region = "eu-west-3"
 
 }
+ data "aws_security_group" "selected"{
+     id = "sg-081aa0ddd83e3c5b5"
+}
+variable "instance_tags" {
+  type = map(string)
+  default = {
+    Name = "subnet-0028c1d92b5b56caa"
+  }
+}
+
+resource "aws_instance" "Iac_instance" {
+  ami = "ami-00983e8a26e4c9bd9"
+  instance_type = "t2.micro"
+  key_name = "AccNew"
+	subnet_id = var.instance_tags.Name
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+  security_groups = [data.aws_security_group.selected.id]
+tags = {
+    Name = " Iac Instance "
+ }
+}
