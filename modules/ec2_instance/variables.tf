5b01022e98d69e073320bab25fe48e09b82acbf7
Lafdi Yahya <yahya.lafdi@edu.uiz.ac.ma>
2023-11-13 18:07:47 +0100

    fixing variable scops

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -7,7 +7,16 @@ variable "instance_name"{
 }
 
 
-
+variable "prefix" {
+	description = "Specifying prefix for your ressources"
+	type = string
+	default = "my-ci-cd"
+}
+variable "zone" {
+	description = "Specifying availibilty zone"
+	type = string
+	default = "eu-west-3"
+}
 
 variable "sg-id"{
 	description = "use sepecific security group"
--- variables.tf
+++ variables.tf
@@ -3,15 +3,5 @@ variable "duck_dns" {
   
 }
 
-variable "prefix" {
-	description = "Specifying prefix for your ressources"
-	type = string
-	default = "my-ci-cd"
-}
-variable "zone" {
-	description = "Specifying availibilty zone"
-	type = string
-	default = "eu-west-3"
-}
 
 
