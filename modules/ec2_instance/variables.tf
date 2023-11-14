c57634bbf819fcd3e8f47daa888fbe7e39a6a0c7
Lafdi Yahya <yahya.lafdi@edu.uiz.ac.ma>
2023-11-14 23:38:42 +0100

    fix vars sysntax

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -41,3 +41,10 @@ variable "ssh_key"{
 	type = string
 	default = "AccNew"  # already created an ssh key on AWS
 }
+variable "duck_dns" {
+	description = "DuckDns token "
+  default = "db8108bc-b996-4f0f-b619-d50d35c9fc75"
+}
+
+
+
