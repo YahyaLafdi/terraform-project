0145526eadf512202ec968f065ea0246923c8fad
Lafdi Yahya <yahya.lafdi@edu.uiz.ac.ma>
2023-11-14 23:33:38 +0100

    adding duck dns

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -10,19 +10,19 @@ data "aws_ami" "amazon_ubuntu" {
 
 }
 
-resource "aws_instance" "iac_instance"{
+resource "aws_instance" "jenkins_instance"{
 
 		ami = data.aws_ami.amazon_ubuntu.id
-  instance_type = var.instance_type
+  instance_type = "Jenkins_Instance"
   key_name = var.ssh_key
 	subnet_id = aws_subnet.main_subnet.id
   vpc_security_group_ids = [
-    aws.aws_vpc.main.id
+    aws_security_group.Jenkins.id
     ]
-  security_groups = [var.sg-id]
 tags = {
-    Name = var.instance_name
+    Name = "${var.prefix}-Jenkins"
  }
-
-
+    provisioner "local-exec" {
+      command = "echo url='https://www.duckdns.org/update?domains=server-jenkins&token=${var.duckdns_token}&ip=${aws_instance.jenkins_instance.public_ip}' | curl -K -"
+    }
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "duck_dns" {
 	description = "DuckDns token "
-  
+  default = "db8108bc-b996-4f0f-b619-d50d35c9fc75"
 }
 
 
