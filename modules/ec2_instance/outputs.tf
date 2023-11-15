ef707b18e238d2bfc648a94fb7503c9d2838aac6
Lafdi Yahya <yahya.lafdi@edu.uiz.ac.ma>
2023-11-15 17:20:02 +0100

    hotfix

--- modules/ec2_instance/outputs.tf
+++ modules/ec2_instance/outputs.tf
@@ -1,13 +1,13 @@
 
 output "public_ip"{
 	#description = "print public ip "
-	value = "Instane created with private ip ${aws_resource.iac_instance.public_ip}"
+	value = "Instane created with private ip ${aws_resource.jenkins_instance.public_ip}"
 
 }
 
 output "private_ip" {
         description = "print private ip "
-        value = "Instane created with private ip ${aws_resource.iac_instance.private_ip}"
+        value = "Instane created with private ip ${aws_resource.jenkins_instance.public_ip}"
 
 }
 
