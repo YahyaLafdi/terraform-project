2b6a806e9a7ae8d1b5e317a4c450c915cbd3ff81
Ubuntu-Yahya <yhylfd@gmail.com>
2023-11-15 16:17:08 +0000

    fixing outpout blocks

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 
-output "Jenkins pubic_ip"{
+output "Jenkins_pubic_ip"{
 	description = "print public ip "
-	value = module.ec2_instance.aws_instance.jenkins_instance.public_ip
+	value = module.ec2_instance.pubic_ip
 
 }
 
 output "private_ip" {
         description = "print private ip "
-        value = module.ec2_instance.aws_instance.jenkins_instance.public_ip
+        value = module.ec2_instance.private_ip
 
 }
 
